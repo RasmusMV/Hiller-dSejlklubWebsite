188eb138df1597eb5bd83b8f3fdac7f29d2e612c
Marc3935 <maj035@edu.zealand.dk>
2025-12-18 10:00:35 +0100

    added extra pages

---
+++ HillerødSejlklubWebsite/Pages/Kalender.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HillerødSejlklubWebsite.Pages
+{
+    public class KalenderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
