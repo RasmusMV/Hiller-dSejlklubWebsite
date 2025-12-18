659b5e6c3e6a66451aaadd85bc641a64f7bf010b
Lennnoss <lennartnice2@gmail.com>
2025-12-18 01:44:22 +0100

    Både, hold, lokationsregler, kontakt page tilføjet men ikke udfyldt endnu

---
+++ HillerødSejlklubWebsite/Pages/Både.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HillerødSejlklubWebsite.Pages
+{
+    public class BådeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
